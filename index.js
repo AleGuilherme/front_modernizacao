6653ef1cf414bb88b6fbed06c2b7eb062654e37d
professorisidro <isidro@professorisidro.com.br>
2021-01-20 14:58:59 -0300

    pagina de comunidades

--- index.js
+++ index.js
@@ -22,7 +22,27 @@ function enviar(){
        res = fetch("http://localhost:8088/login", cabecalho);
        console.log(res);
     */
-    fetch("http://localhost:8088/login", cabecalho).then(res => console.log(res));
+    fetch("http://localhost:8088/login", cabecalho).then(res => trataStatus(res));
+}
 
+
+function trataStatus(res){
+    if (res.status == 200){
+        // preciso extrair os dados da resposta
+        res.json().then(user => {
+            localStorage.setItem("userMod", JSON.stringify(user));  //gravo os dados do usuário no cache
+            window.location = "comunidades.html";  // mudo de página
+        });
+    }
+    else if (res.status == 403){
+        document.getElementById("msgErro").innerHTML = "Senha Inválida!";
+    }
+    else if (res.status == 404){
+        document.getElementById("msgErro").innerHTML = "Usuário não encontrado!";
+    }
+    else {
+        document.getElementById("msgErro").innerHTML = "Erro desconhecido";
+
+    }
 
 }
