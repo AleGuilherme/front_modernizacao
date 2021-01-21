5dc20477a86df066978a88ce6e93f52719304ef9
professorisidro <isidro@professorisidro.com.br>
2021-01-21 11:57:45 -0300

    nova modernizacao

--- novamodernizacao.js
+++ novamodernizacao.js
@@ -1,7 +1,32 @@
+// preciso do ID da comunidade global para várias funções
+var IDCom;
+
+
 function carregaInfo(){
     var userSTR = localStorage.getItem("userMod");
     if (!userSTR){
         window.location = "index.html";
         return;
     }
+
+    // preciso recuperar o ID da comunidade que veio na URL
+    var str = window.location.search;
+    console.log(str);
+    var idComunidade = str.substr(4);
+    console.log("ID da comunidade = "+idComunidade);
+
+    IDCom = parseInt(idComunidade); // já atribuí e converti para inteiro
+
+
+    fetch("http://localhost:8088/comunidades/"+IDCom)
+        .then(res => res.json())
+        .then(comunidade => document.getElementById("nomeComunidade").innerHTML = comunidade.nome);
+
+    /*
+     Como eu leio o fetch?
+     res = fetch("http://localhost:8088/comunidades/"+IDCom)
+     comunidade = res.json();
+     document.getElementById("nomeComunidade").innerHTML = comunidade.nome;
+    */
+
 }
